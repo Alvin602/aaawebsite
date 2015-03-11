579823883ca01a300d60b402b9aef41f134a2308
Alvin Polanco <alvin.polanco@outlook.com>
2015-03-10 22:59:46 -0400

    Fixed Event creation errors.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ActiveRecord::Base
 	validates_presence_of :title, :date, :description
 	has_many :comments, :dependent => :destroy
+  attr_accessible :title, :date, :description, :image
 end
